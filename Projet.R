99e152415f970651db375f145455e29214527634
ZahraZellazi <zahra.zellazi@esprit.tn>
2025-04-25 13:56:03 +0100

    installation des packages

---
+++ Projet.R
@@ -0,0 +1,12 @@
+#istallation des packages 
+install.packages("tidyverse")       # Manipulation de données + ggplot2
+install.packages("lubridate")       # Manipulation des dates
+install.packages("forecast")        # Modélisation ARIMA, auto.arima
+install.packages("tseries")         # Tests statistiques (ADF, etc.)
+install.packages("urca")            # Tests de racine unitaire
+install.packages("tsibble")         # Séries temporelles tidy
+install.packages("feasts")          # Décomposition + analyse graphique
+install.packages("readxl")          # Lire les fichiers Excel
+install.packages("patchwork")       # Combiner des graphiques
+install.packages("fable")           # Modélisation avancée (ARIMA, ETS)
+install.packages("knitr")           # Pour les fichiers R Markdown
