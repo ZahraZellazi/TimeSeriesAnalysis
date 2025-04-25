ad5120222d21f105daa44238f39d255dd25c460a
ZahraZellazi <zahra.zellazi@esprit.tn>
2025-04-25 14:16:17 +0100

    ts creation and verification

--- Projet.R
+++ Projet.R
@@ -18,6 +18,18 @@ for (pkg in packages) {
 production_electronique <- read_excel(file.choose(), skip = 10)
 production_electronique$observation_date <- as.Date(production_electronique$observation_date)
 head(production_electronique)
+tail(production_electronique)
 str(production_electronique)
 summary(production_electronique)
 class(production_electronique)
+# Créer une série temporelle avec fréquence mensuelle
+production_electronique_ts <- ts(production_electronique$IPB53122N,
+                                 start = c(2009, 1),  # Début en janvier 2009
+                                 frequency = 12)      # Fréquence mensuelle
+# Afficher les premières lignes de la série multivariée
+head(production_electronique_ts)
+
+
+# Vérifiez la série temporelle
+plot(production_electronique_ts)
+class(production_electronique_ts)
